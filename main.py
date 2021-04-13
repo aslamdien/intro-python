7e6fac436b31290edd1f8dde9fb97db30d5428c5
aslamdien <aslamdien90@gmail.com>
2021-04-13 08:38:59 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Python task 2
+print("")
+_PI = 3.14
+radius = int(input("Please enter the radius:"))
+area = _PI * radius ** 2
+print("the area of the circle is:" + str(area))
+
+# Python Task 3
+circumference = 2 * _PI * radius
+print("the circumference of the circle is:" + str(circumference))
+diameter = 2 * radius
+print("the diameter of a circle is:" + str(diameter))
+# intro-python1
